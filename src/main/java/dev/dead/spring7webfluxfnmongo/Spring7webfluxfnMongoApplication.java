bcc0ad55e3857d35644ec343b83d78ea95b831d6
Ahmed M. Ali <ahmedmohammed199975@gmail.com>
2026-01-05 05:45:33 +0300

    init

---
+++ src/main/java/dev/dead/spring7webfluxfnmongo/Spring7webfluxfnMongoApplication.java
@@ -0,0 +1,13 @@
+package dev.dead.spring7webfluxfnmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring7webfluxfnMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring7webfluxfnMongoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/dead/spring7webfluxfnmongo/Spring7webfluxfnMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dead.spring7webfluxfnmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring7webfluxfnMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
